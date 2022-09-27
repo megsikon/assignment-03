4aa1362163fe3da9a309c74a6bdae50dcc80301b
Magnus Emil Gervig Svensson <mesv@itu.dk>
2022-09-27 12:32:33 +0200

    cs exercise 3-5

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -2,4 +2,10 @@ namespace Assignment3.Entities;
 
 public class Task
 {
+    public int Id {get; set;}
+    public string Title {get; set;}
+    public User? AssignedTo {get; set;}
+    public string? Description {get; set;}
+    public State State {get; set;}
+    public ICollection<Tag> Tags {get; set;}
 }
---
+++ Assignment3.Entities/Usings.cs
@@ -0,0 +1,5 @@
+global using Assignment3.Core;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+global using static Assignment3.Core.State;
