5ad8946087cc7f1bb2061a68f0e3852bf05f5be7
katrinesando <kmsa@itu.dk>
2022-09-27 12:32:33 +0200

    implemented fields in Entitety folder

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,5 +1,14 @@
 namespace Assignment3.Entities;
 
+using System.ComponentModel.DataAnnotations;
 public class Task
 {
+    public int Id{get;set;}
+
+    [StringLength(100)]
+    public string Title{get;set;}
+    public User AssignedTo{get;set;}
+    public string Description {get;set;}
+    
+
 }
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,5 +1,13 @@
 namespace Assignment3.Entities;
-
+using System.ComponentModel.DataAnnotations;
 public class User
 {
+    public virtual int Id{get;set;}
+    [StringLength(100)]
+    public virtual string Title{get;set;}
+    
+    [StringLength(100)]
+    [Key]
+    public virtual string Email{get;set;}
+
 }
