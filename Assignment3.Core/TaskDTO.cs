2497be007fdb0f8774cfcadf6931a7b8fa2414fc
Magnus Emil Gervig Svensson <mesv@itu.dk>
2022-09-27 12:33:25 +0200

    Co-authored-by: katrinesando <katrinesando@users.noreply.github.com> Co-authored-by: hannahcandersen <hannahcandersen@users.noreply.github.com>

--- Assignment3.Core/TaskDTO.cs
+++ Assignment3.Core/TaskDTO.cs
@@ -4,6 +4,6 @@ public record TaskDTO(int Id, string Title, string AssignedToName, IReadOnlyColl
 
 public record TaskDetailsDTO(int Id, string Title, string Description, DateTime Created, string AssignedToName, IReadOnlyCollection<string> Tags, State State, DateTime StateUpdated);
 
-public record TaskCreateDTO([StringLength(100)]string Title, int? AssignedToId, string? Description, ICollection<string> Tags);
+public record TaskCreateDTO([Required, StringLength(100)]string Title, int? AssignedToId, string? Description, ICollection<string> Tags);
 
-public record TaskUpdateDTO(int Id, [StringLength(100)]string Title, int? AssignedToId, string? Description, ICollection<string> Tags, State State);
+public record TaskUpdateDTO(int Id, [Required, StringLength(100)]string Title, int? AssignedToId, string? Description, ICollection<string> Tags, State State);
